277b6357ce50b0241055c756967cf522e2dfc076
sinderpl <sinderpl@gmail.com>
2017-01-03 13:22:47 +0000

    Reading in classes

---
+++ ReflectionAPI/src/ie/gmit/sw/Metric.java
@@ -0,0 +1,12 @@
+package ie.gmit.sw;
+
+/**
+ * 
+ * @author G00313177
+ * 
+ * 
+ *
+ */
+public class Metric {
+
+}
---
+++ ReflectionAPI/src/ie/gmit/sw/gui/Runner.java
@@ -0,0 +1,13 @@
+package ie.gmit.sw.gui;
+
+
+
+public class Runner {
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new AppWindow();
+			}
+		});
+	}
+}
